f1b5e81d76b7ef11d507a1f7fa94f7ae7ee0612e
Trojanzaro <anastasiszaro@gmail.com>
2020-12-14 16:53:57 +0200

    Added an extra case

--- src/test/java/org/example/RomanNumeralsTest.java
+++ src/test/java/org/example/RomanNumeralsTest.java
@@ -11,6 +11,12 @@ public class RomanNumeralsTest {
     }
 
     @Test
+    public void testForWrongPlacement() {
+        int number = RomanNumerals.parse("IXV");
+        Assert.assertEquals(14, number);
+    }
+
+    @Test
     public void testForCorrectInputNumerals() {
         int number = RomanNumerals.parse("MDC");
         Assert.assertEquals(1600, number);
